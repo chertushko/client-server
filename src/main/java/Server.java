a4324abc74c468d2ab7e063040a39fd4711ad21a
chertushko <chertushko@gmail.com>
2022-04-15 22:55:17 +0300

    try-catch

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -8,12 +8,13 @@ import java.net.Socket;
 public class Server {
     public static void main(String[] args) {
         int port = 9099;
+
         while (true) {
-            try {
-                ServerSocket serverSocket = new ServerSocket(port);
-                Socket clientSocket = serverSocket.accept();
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            try (ServerSocket serverSocket = new ServerSocket(port);
+                 Socket clientSocket = serverSocket.accept();
+                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+
                 System.out.println("New connection accepted");
                 final String name = in.readLine();
                 out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
